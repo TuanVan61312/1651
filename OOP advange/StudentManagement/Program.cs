2f762f0750ba739ec0f1eba3247bc3a418a0fbe1
TuanVan61312 <vantuan01219230619@gmail.com>
2022-11-24 17:16:02 +0700

    34/11/2022

--- OOP advange/StudentManagement/Program.cs
+++ OOP advange/StudentManagement/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StudentManagement;
+
+bool running = true;
+School gw = new School();
+while(running)
+{
+    gw.PrintMenu();
+    int choice = gw.GetChoice();
+    gw.Process(choice);
+    running = (choice != 0);
+}
