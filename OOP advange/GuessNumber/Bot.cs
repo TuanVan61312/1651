dd7e4d7aa3b3bf90d115f50b6e1f3c518b6803b3
TuanVan61312 <vantuan01219230619@gmail.com>
2022-11-29 09:34:04 +0700

    29/11/2022

---
+++ OOP advange/GuessNumber/Bot.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    public class Bot : Player
+    {
+        public Bot() : base("Bot")
+        {
+        }
+
+        public override void GenerateNumber()
+        {
+            number = new Random().Next(1, 6);
+        }
+
+        public override int GuessNumber()
+        {
+            int n = new Random().Next(1, 6);
+            System.Console.WriteLine("Bot guess the number: " + n);
+            return n;
+        }
+
+        public override int Bet()
+        {
+            int bet = new Random().Next(1, balance + 1);
+            System.Console.WriteLine("Bot bet: " + bet);
+            return bet;
+        }
+    }
+}
