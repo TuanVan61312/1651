61af22811357b8843a34056ecafd7283cbf76cd8
TuanVan61312 <vantuan01219230619@gmail.com>
2022-12-08 07:40:19 +0700

    awdasd

--- OOP advange/DemoExeption2/Program.cs
+++ OOP advange/DemoExeption2/Program.cs
@@ -1,2 +1,38 @@
 ﻿using System;
 
+namespace DemoExeption2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = ReadInt("Enter a: ");
+            int b = ReadInt("Enter b: ");
+            int c = a/b;
+            System.Console.WriteLine("Result: " + c);
+        }
+        public static int ReadInt(string prompt)
+        {
+            bool invalid = true;
+            int n = 0;
+            while(invalid)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    n = int.Parse(Console.ReadLine());
+                    invalid = false;
+                }
+                catch (FormatException)
+                {
+                    System.Console.WriteLine("Invalid input try agian");
+                }
+                catch (DivideByZeroException)
+                {
+                    System.Console.WriteLine("DivideByZeroException");
+                }
+            }
+            return 0;
+        }
+    }
+}
